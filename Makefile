493d8168fb767d1e1303928fa1706ea3227024cf
Michael Serebrennikov <aleus@rambler.ru>
2014-07-27 00:12:54 +0900

    Реализовал обработку запросов

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ PROTO_DIR := ./k12http_proto
 k12http: $(GO_FILES) 
 	    @echo Building "$(PROGRAM)"
 		@go build
+		./$(PROGRAM)
 
 clean:
 	    rm -f $(PROGRAM) $(PROTO_DIR)/$(PROGRAM).pb.go *.log
--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 import "net/http"
-import "html"
 import "log"
+import "time"
+
+const layout = "15:04 02.01"
 
 func handleGET (response_writer http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf (response_writer, "Hello, %q", html.EscapeString(r.URL.Path))
+    values := r.URL.Query()
+    fmt.Fprintf (response_writer, "URL: %s\n", r.URL.Path)
+    fmt.Fprintf (response_writer, "user: %s\n", values["user"][0])
+    fmt.Fprintf (response_writer, "message: %s\n", values["message"][0])
+    fmt.Fprintf (response_writer, "time: %s\n", time.Now().Format(layout))
 }
 
 func main() {
